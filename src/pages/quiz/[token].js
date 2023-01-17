252f0cc17f53c2014a372010c783313496113eee
Jeff Mac <jeff@Jeffs-MacBook-Air.local>
2023-01-17 19:57:18 -0300

    dynamic routes for quizzes

--- src/pages/quiz/[token].js
+++ src/pages/quiz/[token].js
@@ -10,14 +10,22 @@ export async function getStaticProps({ params }) {
 export async function getStaticPaths() {
 	const quizRef = collection(db, "quizzes");
 	const quizzes = await getDocs(quizRef);
-	console.log(quizzes)
+	console.log(quizzes);
 	const paths = quizzes.docs.map((doc) => ({
 		params: { token: doc.id },
 	}));
-	console.log(paths)
+	console.log(paths);
 	return { paths, fallback: false };
 }
 
 export default function QuizPage({ quiz }) {
-	return <div>{quiz.description}</div>;
+	return (
+		<>
+			<div>{quiz.description}</div>
+			{/* TODO los arrays de preguntas se estan guardando como strings */}
+			{/* {quiz.questions.map((question, index) => (
+				<div key={question.index}>{question.question}</div>
+			))} */}
+		</>
+	);
 }
