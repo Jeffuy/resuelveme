6a21f515cbc435215be1155a2a5153108184cbdc
Jeff Free <jefffreelance3@gmail.com>
2023-03-13 01:48:49 -0300

    edit user photo fixed

---
+++ src/app/quizTry/[token]/page.jsx
@@ -0,0 +1,80 @@
+"use client"
+import "@styles/quiz.css"
+import QuizContent from "@components/quiz/QuizContent";
+import QuizStatics from "@components/quiz/QuizStatics";
+import QuizRanking from "@components/quiz/QuizRanking";
+import { useEffect, useState, useContext } from "react";
+import { db, auth } from "@firebase/firebase";
+import { AuthContextProvider } from "@context/AuthContext";
+import { QuizContextProvider } from "@context/QuizContext";
+//import { useRouter } from "next/router";
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	updateDoc,
+} from "firebase/firestore";
+
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+
+
+export default function QuizPage({ quiz }) {
+	const [showRanking, setShowRanking] = useState(false);
+	const [showStats, setShowStats] = useState(false);
+	const showRankingDiv = () => {
+		setShowStats(false);
+		setShowRanking(!showRanking);
+	}
+	const showStatsDiv = () => {
+		setShowRanking(false);
+		setShowStats(!showStats);
+	}
+	console.log(quiz.title)
+
+	return (
+		<AuthContextProvider>
+			<QuizContextProvider>
+				<QuizContent quiz={quiz} />
+				<div className="actionsContainerButtons">
+					<button onClick={showRankingDiv} className="buttonShowRankStats">{ 'Show ranking'}</button>
+					<button onClick={showStatsDiv} className="buttonShowRankStats">{ 'Show ranking'}</button>
+				</div>
+				<QuizStatics quiz={quiz} show={showStats}/>
+				<QuizRanking quiz={quiz} show={showRanking} />
+			</QuizContextProvider>
+		</AuthContextProvider>
+	)
+}
+
+export async function generateStaticParams() {
+	const quizRef = collection(db, "quizzes");
+	const quizSnapshot = await getDocs(quizRef);
+	const quizIds = quizSnapshot.docs.map((doc) => doc.uid);
+	return {
+		paths: quizIds.map((id) => ({ params: { token: id } })),
+		fallback: false,
+	};
+}
+
+export async function getServerSideProps({ params }) {
+	const quizRef = collection(db, "quizzes");
+	const quiz = await getDoc(doc(quizRef, params.token));
+	const createdAt = quiz.data().createdAt;
+	let solvers = null
+	if (quiz.data().solvers) {
+		solvers = quiz.data().solvers;
+		for (let i = 0; i < solvers.length; i++) {
+			let date = solvers[i].date;
+			if(typeof date === 'object') {
+			solvers[i].date = date.toDate().toLocaleTimeString('es-UY', { year: 'numeric', month: 'long', day: 'numeric' });
+			}
+		}
+		quiz.data().solvers = solvers;
+	}
+
+	return { params: { quiz: { ...quiz.data(), createdAt: createdAt.toDate().toLocaleTimeString('es-UY', { year: 'numeric', month: 'long', day: 'numeric' }), solvers: solvers } } };
+}
--- src/components/dashboard/DashboardMain.jsx
+++ src/components/dashboard/DashboardMain.jsx
@@ -7,41 +7,46 @@ import { useRouter } from 'next/navigation';
 import { storage, auth, db } from '@firebase/firebase.js';
 import { ref as storageRef, getDownloadURL, uploadBytes } from 'firebase/storage';
 import { useUpdateProfile } from 'react-firebase-hooks/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
 import Image from 'next/image';
 
 const DashboardMain = () => {
 	const router = useRouter();
 	const { user, loading, logout, userData, userDataLoading } = useContext(AuthContext);
 
-	const profilePicture = storageRef(storage, `users/${user?.uid}/profilePicture.jpeg`);
+	//const profilePicture = storageRef(storage, `users/${user?.uid}/profilePicture.jpeg`);
 
-	const profilePictureSmall = storageRef(storage, `users/${user?.uid}/profilePictureSmall.jpeg`);
+	//const profilePictureSmall = storageRef(storage, `users/${user?.uid}/profilePictureSmall.jpeg`);
 	const [edit, setEdit] = useState(false);
 
 	// eslint-disable-next-line no-unused-vars
 	const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
 
-	const [editPhotoUrl, setEditPhotoUrl] = useState(false);
+	
 	const [selectedFile, setSelectedFile] = useState(null);
-	const [imageURL, setImageURL] = useState(userData?.photoURL);
 	const [previewURLimage, setPreviewURLimage] = useState('');
+	const [username, setUsername] = useState(userData?.username);
 
 	const [error, setError] = useState("");
 
 	async function updatePhotoSmall(photoSmall) {
-		await setDoc(doc(db, 'users', user.uid), { profilePictureSmall: photoSmall }, { merge: true });
+		await updateDoc(doc(db, 'users', user.uid), { profilePictureSmall: photoSmall });
 	}
 
 	async function updatePhotoUrl(photoURL) {
-		await setDoc(doc(db, 'users', user.uid), { profilePicture: photoURL }, { merge: true });
+		await updateDoc(doc(db, 'users', user.uid), { profilePicture: photoURL });
 	}
 
-	const upload = async (selectedFile, ref, size) => {
+	const upload = async (selectedFile, size) => {
 		if (selectedFile) {
+			let ref;
+			if (size == 'big') {
+				ref = storageRef(storage, `users/${user?.uid}/profilePicture.jpeg`);
+			} else if (size == 'small') {
+				ref = storageRef(storage, `users/${user?.uid}/profilePictureSmall.jpeg`);
+			}
 			const result = await uploadBytes(ref, selectedFile).then(snapshot => {
 				getDownloadURL(snapshot.ref).then(url => {
-					setImageURL(url);
 					if (size == 'big') {
 						updateProfile({ photoURL: url });
 						updatePhotoUrl(url);
@@ -56,120 +61,126 @@ const DashboardMain = () => {
 		}
 	};
 
-	// const handleSubmit = async (e) => {
-	// 	e.preventDefault();
-	// 	if (selectedFile) {
-	// 		const image = selectedFile;
-	// 		new Compressor(image, {
-	// 			quality: 0.2,
-	// 			success: compressedResult => {
-	// 				upload(compressedResult, profilePicture, 'big');
-	// 			},
-	// 		});
-
-	// 		const imageSmall = selectedFile;
-	// 		new Compressor(imageSmall, {
-	// 			quality: 0.05,
-	// 			success: compressedResult2 => {
-	// 				upload(compressedResult2, profilePictureSmall, 'small');
-	// 			},
-	// 		});
-	// 	}
-	// 	setSelectedFile(null);
-	// 	setEditPhotoUrl(false);
-	// };
-
 	const handleSubmit = async (e) => {
 		e.preventDefault()
-		setEdit(!edit);
+
+		if (username != userData?.username && username != '') {
+			await updateDoc(doc(db, 'users', user.uid), { username: username });
+		}
+
 		console.log(e);
-		if (previewURLimage != '') {
-			console.log("Cambio la imagens")
-		} else {
+		if (selectedFile) {
+			const image = selectedFile;
+			new Compressor(image, {
+				quality: 0.2,
+				success: compressedResult => {
+					upload(compressedResult, 'big');
+				},
+			});
+
+			const imageSmall = selectedFile;
+			new Compressor(imageSmall, {
+				quality: 0.05,
+				success: compressedResult2 => {
+					upload(compressedResult2, 'small');
+				},
+			});
+			setSelectedFile(null);
+		}
+		else {
 			console.log("no cambio la imagen")
 		}
-	}
+	setEdit(!edit);
+}
 
-	useEffect(() => {
-		userData?.profilePicture === "https://firebasestorage.googleapis.com/v0/b/resuelveme-9e9bb.appspot.com/o/users%2Fundefined%2FprofilePicture.jpeg?alt=media&token=0d5c9a21-6767-4687-8063-73d04a44e30d" ? null :
-			getDownloadURL(profilePicture).then(url => setImageURL(url));
-	}, [userData])
 
-	if (loading || userDataLoading) {
-		return <div className="loaderContainer"><span className="loader"></span></div>;
-	}
+// useEffect(() => {
+// 	if (userData) {
+// 		userData?.profilePicture === "https://firebasestorage.googleapis.com/v0/b/resuelveme-dev.appspot.com/o/users%2Fundefined%2FprofilePicture.jpeg?alt=media&token=8ba8c063-44ab-4831-be3e-c0c9542ed826" ? null :
+// 			getDownloadURL(profilePicture).then(url => setImageURL(url));
+// 	}
+// }, [userData?.profilePicture])
 
-	if (!user) {
-		router.push('/login');
-		return <div className="loaderContainer"><span className="loader"></span></div>;
-	}
+if (loading || userDataLoading) {
+	return <div className="loaderContainer"><span className="loader"></span></div>;
+}
 
-	const openFileSelected = async () => {
-		document.getElementById('inputFile').click();
-	}
+if (!user) {
+	router.push('/login');
+	return <div className="loaderContainer"><span className="loader"></span></div>;
+}
 
-	return (
-		<>
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-			<div>
-				{userData &&
-					<div className="imageContainer">
-						<Image src={userData.profilePicture && previewURLimage == '' ? userData.profilePicture : previewURLimage} width={200} height={200} alt="profile picture" />
-						{edit && <button type="button" onClick={openFileSelected}>
-							<i className="fa fa-pencil"></i>
-						</button>}
-					</div>
-				}
+const openFileSelected = async () => {
+	document.getElementById('inputFile').click();
+}
 
-				{!edit && user && <h1>{userData?.username}</h1>}
+const cancelEdit = async () => {
+	setPreviewURLimage('');
+	setEdit(!edit);
+}
 
-				<form onSubmit={handleSubmit} className={edit ? 'editForm' : null} >
-					<input type="file" onChange={e => {
-						const file = e.target.files ? e.target.files[0] : undefined;
-						setSelectedFile(file);
-						setPreviewURLimage(URL.createObjectURL(file));
-					}}
-						id="inputFile"
-						style={{ display: 'none' }}
-					/>
-					{error && <p>{error}</p>}
-					{!edit && user &&
-						<>
-							<li><label className='dataLabel'>Member Since : <span>{userData?.createdAt?.toDate().toLocaleDateString('es-ES')}</span></label></li>
-							<li><label className='dataLabel'>Solved Quizzes : <span>{userData?.solvedQuizzes?.length || 0}</span></label></li>
-							<li><label className='dataLabel'>Created Quizzes : <span>{userData?.createdQuizzes?.length || 0}</span></label></li>
-							<li><label className='dataLabel'>Played Quizzes : <span>{userData?.playedQuizzes?.length || 0}</span></label></li>
-							<li><label className='dataLabel'>Total attempts : <span>{userData?.attempts || 0}</span></label></li>
-							<li><label className='dataLabel'>Correct attempts : <span>{userData?.successAttempts || 0}</span></label></li>
-							<li><label className='dataLabel'>Correct answers ratio : <span>{userData?.attempts ? Math.round((userData?.successAttempts / userData?.attempts) * 100) : 0}%</span></label></li>
-							<li><label className='dataLabel'>Solved ratio : <span>
-								{userData?.solvedQuizzes?.length ? Math.round((userData?.solvedQuizzes.length / userData?.playedQuizzes?.length) * 100) : 0}
-								%</span></label></li>
-						</>
-					}
-					{edit && user &&
-						<>
-							<label htmlFor="">Username</label>
-							<input type="text" />
-							<label htmlFor="">Email</label>
+return (
+	<>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+		<div>
+			{userData &&
+				<div className="imageContainer">
+					<Image priority src={userData.profilePicture && previewURLimage == '' ? userData.profilePicture : previewURLimage} width={200} height={200} alt="profile picture" />
+					{edit && <button type="button" onClick={openFileSelected}>
+						<i className="fa fa-pencil"></i>
+					</button>}
+				</div>
+			}
+
+			{!edit && user && <h1>{userData?.username}</h1>}
+			{!edit && user &&
+				<>
+					<li><label className='dataLabel'>Member Since : <span>{userData?.createdAt?.toDate().toLocaleDateString('es-ES')}</span></label></li>
+					<li><label className='dataLabel'>Solved Quizzes : <span>{userData?.solvedQuizzes?.length || 0}</span></label></li>
+					<li><label className='dataLabel'>Created Quizzes : <span>{userData?.createdQuizzes?.length || 0}</span></label></li>
+					<li><label className='dataLabel'>Played Quizzes : <span>{userData?.playedQuizzes?.length || 0}</span></label></li>
+					<li><label className='dataLabel'>Total attempts : <span>{userData?.attempts || 0}</span></label></li>
+					<li><label className='dataLabel'>Correct attempts : <span>{userData?.successAttempts || 0}</span></label></li>
+					<li><label className='dataLabel'>Correct answers ratio : <span>{userData?.attempts ? Math.round((userData?.successAttempts / userData?.attempts) * 100) : 0}%</span></label></li>
+					<li><label className='dataLabel'>Solved ratio : <span>
+						{userData?.solvedQuizzes?.length ? Math.round((userData?.solvedQuizzes.length / userData?.playedQuizzes?.length) * 100) : 0}
+						%</span></label></li>
+				</>
+			}
+
+			<form onSubmit={handleSubmit} className={edit ? 'editForm' : null} >
+				<input type="file" onChange={e => {
+					const file = e.target.files ? e.target.files[0] : undefined;
+					setSelectedFile(file);
+					setPreviewURLimage(URL.createObjectURL(file));
+				}}
+					id="inputFile"
+					style={{ display: 'none' }}
+				/>
+				{error && <p>{error}</p>}
+				{edit && user &&
+					<>
+						<label htmlFor="">Username</label>
+						<input type="text" defaultValue={userData.username} onChange={e => setUsername(e.target.value)} />
+						{/* <label htmlFor="">Email</label>
 							<input type="text" />
 							<label htmlFor="">New password</label>
 							<input type="password" />
 							<label htmlFor="">Repeat new password</label>
-							<input type="password" />
-						</>
-					}
-					<input type="submit" style={{ display: 'none' }} id="submitButton" />
-				</form>
-				<div className="actionsContainer">
-					{user && !edit && <button onClick={() => setEdit(!edit)} className="edit">Edit</button>}
-					{user && edit && <button onClick={() => setEdit(!edit)} className="edit">Cancel</button>}
-					{edit && user && <button onClick={() => document.getElementById('submitButton').click()} className="save">Save</button>}
-					{user && !edit && <button onClick={() => logout()} className="logout">Logout</button>}
-				</div>
+							<input type="password" /> */}
+					</>
+				}
+				<input type="submit" style={{ display: 'none' }} id="submitButton" />
+			</form>
+			<div className="actionsContainer">
+				{user && !edit && <button onClick={() => setEdit(!edit)} className="edit">Edit</button>}
+				{user && edit && <button onClick={cancelEdit} className="edit">Cancel</button>}
+				{edit && user && <button onClick={() => document.getElementById('submitButton').click()} className="save">Save</button>}
+				{user && !edit && <button onClick={() => logout()} className="logout">Logout</button>}
 			</div>
-		</>
-	);
+		</div>
+	</>
+);
 };
 
 export default DashboardMain;
